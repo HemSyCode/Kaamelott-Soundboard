603c4b4dfe39cfd72901dc7e67162603ad73f923
HemSyCode <Syldu56@live.fr>
2022-09-03 20:21:27 +0200

    Build app error fixed.

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import LanguageDetector from "i18next-browser-languagedetector"
 import {initReactI18next} from 'react-i18next'
-import XHR from 'i18next-xhr-backend'
+import XHR from 'i18next-http-backend'
 import languageEN from './locate/en/translate.json'
 import languageFR from './locate/fr/translate.json'
 
